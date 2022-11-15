099523f484b4507dc46a1892a51ae447d2846f46
Murali Krishnan M T <mtmuralikrishnan@gmail.com>
2022-11-15 15:15:59 +0530

    Fast Laugh Page UI Completed

--- lib/presentation/fast_laugh/screen_fast_laugh.dart
+++ lib/presentation/fast_laugh/screen_fast_laugh.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone_bloc/presentation/fast_laugh/widgets/video_list_item.dart';
 
 class ScreenFastLaugh extends StatelessWidget {
   const ScreenFastLaugh({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('FastLaugh'),
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          scrollDirection: Axis.vertical,
+          children: List.generate(10, (index) {
+            return VideoListItem(
+              index: index,
+            );
+          }),
+        ),
+      ),
     );
   }
 }
---
+++ lib/presentation/fast_laugh/widgets/video_list_item.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_bloc/core/colors/colors.dart';
+import 'package:netflix_clone_bloc/core/colors/constants.dart';
+
+class VideoListItem extends StatelessWidget {
+  final int index;
+  const VideoListItem({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: Colors.accents[index % Colors.accents.length],
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 10,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //left side
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.black.withOpacity(0.5),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.volume_off,
+                      color: kWhiteColor,
+                      size: 30,
+                    ),
+                  ),
+                ),
+                //right side
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg'),
+                      ),
+                    ),
+                    VideoActionsWidget(
+                      icon: Icons.emoji_emotions,
+                      title: 'LOL',
+                    ),
+                    VideoActionsWidget(
+                      icon: Icons.add,
+                      title: 'My List',
+                    ),
+                    VideoActionsWidget(
+                      icon: Icons.share,
+                      title: 'Share',
+                    ),
+                    VideoActionsWidget(
+                      icon: Icons.play_arrow,
+                      title: 'Play',
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class VideoActionsWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  const VideoActionsWidget({
+    super.key,
+    required this.icon,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 30,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              color: kWhiteColor,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
